5fd44b6c1acba607ff0cc0e96576297d78248790
seriouslucy <seriouslucy@outlook.com>
2025-01-18 12:08:02 -0600

    modified
    practice

---
+++ javascriptlessons/script.js
@@ -0,0 +1,36 @@
+// alert('do NOT look behind you');
+
+// alert('i am serious');
+
+
+// var myVariable = 0
+// let myVariable = 0 -- can be changed
+// const myVariable = 0 -- constant, no change
+
+const originalPrice = 120
+
+
+let inventory = 100
+inventory -= 5
+inventory += 20
+console.log(inventory)
+
+const price = 10;
+const tax = 0.07;
+const total = price + price * tax;
+console.log(`Total price: $${total.toFixed(2)}`);
+
+let name = document.getElementById("name");
+let output = document.getElementById("output");
+
+function greet(name) {
+
+    alert('Hello' + name + '!');
+}
+
+console.log(2 + 3);
+
+
+
+
+
