e430882e4c2452a9a8a6c42c630a0f4127285b78
seriouslucy <seriouslucy@outlook.com>
2025-01-20 16:45:10 -0600

    updated
    im coding

--- javascriptlessons/script.js
+++ javascriptlessons/script.js
@@ -7,18 +7,8 @@
 // let myVariable = 0 -- can be changed
 // const myVariable = 0 -- constant, no change
 
-let price = 200;
-let discountRate = 0.15;
-let taxRate = .08;
-
-let discount = price * discountRate;
-let discountPrice = price - discount;
-let tax = discountPrice * taxRate;
-let finalPrice = discountPrice + tax;
 
 
-
-// let originalPrice = 120;
 
 
 // let inventory = 100;
@@ -31,15 +21,7 @@ let finalPrice = discountPrice + tax;
 // const total = price + price * tax;
 // console.log(`Total price: $${total.toFixed(2)}`);
 
-let name = document.getElementById("myName").name;
-let output = document.getElementById("output");
 
-function display() {
-    var x = document.getElementById("myName").name;
-    alert('Hello');
-}
-
-console.log(2 + 3);
 
 
 
