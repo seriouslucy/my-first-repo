12b94c02e446ade504055532cebf576c27566129
seriouslucy <seriouslucy@outlook.com>
2025-01-20 12:36:47 -0600

    updated
    rock paper scissors function practice

--- javascriptlessons/script.js
+++ javascriptlessons/script.js
@@ -7,18 +7,29 @@
 // let myVariable = 0 -- can be changed
 // const myVariable = 0 -- constant, no change
 
-const originalPrice = 120
+let price = 200;
+let discountRate = 0.15;
+let taxRate = .08;
+
+let discount = price * discountRate;
+let discountPrice = price - discount;
+let tax = discountPrice * taxRate;
+let finalPrice = discountPrice + tax;
 
 
-let inventory = 100
-inventory -= 5
-inventory += 20
-console.log(inventory)
 
-const price = 10;
-const tax = 0.07;
-const total = price + price * tax;
-console.log(`Total price: $${total.toFixed(2)}`);
+// let originalPrice = 120;
+
+
+// let inventory = 100;
+// inventory -= 5;
+// inventory += 20;
+// console.log(inventory)
+
+// const price = 10;
+// const tax = 0.07;
+// const total = price + price * tax;
+// console.log(`Total price: $${total.toFixed(2)}`);
 
 let name = document.getElementById("myName").name;
 let output = document.getElementById("output");
